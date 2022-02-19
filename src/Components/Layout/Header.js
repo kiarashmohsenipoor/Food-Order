ff7066e65c8930edec56022d89c1e90733f0d31b
kiarash <kiarashmohsenipoor99@gmail.com>
2022-02-19 22:47:38 +0330

    complete header files

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,12 +1,15 @@
 import { Fragment} from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import meals from '../../assets/meals3.webp';
 import styles from './Header.module.css'
 
 const Header = () => {
     return (
         <Fragment>
-            <header className={styles.header}>React Meals</header>
-            <button>Cart</button>
+            <header className={styles.header}>
+                <h1>ReactMeals</h1>
+                <HeaderCartButton />
+            </header>
             <div className={styles['main-image']}>
                 <img src={meals} alt='delicious food'/>
             </div>
