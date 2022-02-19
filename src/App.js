ce6404b0c10d8a924caa172d6768d5fedccfd8c1
kiarash <kiarashmohsenipoor99@gmail.com>
2022-02-19 23:16:59 +0330

    Add meals and meals list to show

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Fragment} from "react";
 import './App.css';
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
     return (
         <Fragment>
-            <Header />
+            <Header/>
+            <main>
+                <Meals/>
+            </main>
         </Fragment>
     );
 }
